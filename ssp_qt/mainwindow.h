c36b49f76844d269c6a0f99ae23a026de899da2f
ZiXuanLing <2226958871@qq.com>
2022-02-02 23:47:48 +0800

    qt

--- ssp_qt/mainwindow.h
+++ ssp_qt/mainwindow.h
@@ -3,10 +3,12 @@
 
 #include <QMainWindow>
 #include <QMessageBox>
-
-#include <QTcpSocket>
+#include <QTableWidgetItem>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-
+//#include "" 此处要包含mess_define.proto
+#include "/home/liuhao/ssp/common/mess_type.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -34,9 +36,21 @@ private:
 private:
     int sock_client;
     bool is_logined;
+    int user_id;
 private:
     int SocketInit();
     int SocketRecv();
     int SocketClose();
+    int RecvRep();
+    char recv_buffer[10240];
+    char send_buffer[10240];
+    ssp::LoginReq loginReq;
+    ssp::LoginRep loginRep;
+    ssp::LogoutReq logoutReq;
+    ssp::LogoutRep logoutRep;
+    ssp::PublishMessageReq publishMessageReq;
+    ssp::PublishMessageRep publishMessageRep;
+    ssp::GetMessageReq getMessageReq;
+    ssp::GetMessageRep getMessageRep;
 };
 #endif // MAINWINDOW_H
